8add8088ffbfc83deffe303197c93d77d8ebffc1
Érico <ericogr@gmail.com>
2016-02-14 11:16:40 -0200

    adicionar algumas validações

--- calculadora.py
+++ calculadora.py
@@ -20,9 +20,16 @@ def multiplicacao(a, b):
 def divisao(a, b):
 	return a / b
 
-def imprime_pilha(pilha):
+def imprimepilha(pilha):
 	for i, p in enumerate(pilha):
 		print('{0}: {1}'.format(i, p))
+
+def iscomandosair(comando):
+	comando = comando.strip().lower()
+	return comando == 'exit' or comando == 'sair' or comando == 's'
+
+def isnumber(number):
+	return number.replace('.', '', 1).isdigit()
 
 operacoes = {
 	'+': adicao,
@@ -34,16 +41,22 @@ operacoes = {
 while True:
 	leitura = input('>>>')
 
-	if not leitura:
-		break
-
 	operacao = operacoes.get(leitura)
 
 	if operacao:
-		valor = operacao(pilha.pop(), pilha.pop())
-		pilha.append(valor)
+		if len(pilha) > 1:
+			valor = operacao(pilha.pop(), pilha.pop())
+			pilha.append(valor)
+		else:
+			print('pilha sem elementos para operacao')
 	else:
-		valor = float(leitura)
-		pilha.append(valor)
+		if iscomandosair(leitura):
+			break
 
-	imprime_pilha(pilha)
+		if isnumber(leitura):
+			valor = float(leitura)
+			pilha.append(valor)
+		else:
+			print('valor invalido: \'{0}\''.format(leitura))
+
+	imprimepilha(pilha)
