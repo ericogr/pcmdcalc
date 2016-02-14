550bbb0fd2603e789d2f13729f742fa12738829a
Érico <ericogr@gmail.com>
2016-02-14 15:48:04 -0200

    adicionar suporte a nr negativos na pilha

--- calculadora.py
+++ calculadora.py
@@ -1,109 +1,128 @@
+#!/usr/bin/env python3
+
 from math import sqrt as sq
 from math import log2 as l2
 
-print("""
-	Calculadora com pilha
-		Digite o valor e ENTER para adicionar na pilha
-		Utilize +, -, * e / para as 4 operações matemáticas básicas
-""")
+def main():
+	print("""
+		Calculadora com pilha
+			Digite o valor e ENTER para adicionar na pilha
+			Utilize +, -, * e / para as 4 operações matemáticas básicas
+			Utilize sq, l2 para raiz quadrada e log2
+	""")
 
-leitura = ''
-valor = 0
-pilha = []
+	leitura = ''
+	valor = 0
+	pilha = []
 
-#############
-# operações #
-#############
-def adicao(a, b):
-	return a + b
+	#############
+	# operações #
+	#############
+	def adicao(a, b):
+		return a + b
 
-def subtracao(a, b):
-	return a - b
+	def subtracao(a, b):
+		return a - b
 
-def multiplicacao(a, b):
-	return a * b
+	def multiplicacao(a, b):
+		return a * b
 
-def divisao(a, b):
-	return a / b
+	def divisao(a, b):
+		return a / b
 
-def power(a, b):
-	return a ** b
+	def power(a, b):
+		return a ** b
 
-def square(a):
-	return sq(a)
+	def square(a):
+		return sq(a)
 
-def log2(a):
-	return l2(a)
+	def log2(a):
+		return l2(a)
 
-############
-# diversos #
-############
-def imprimepilha(pilha):
-	for i, p in enumerate(pilha):
-		print('{0}: {1}'.format(i, p))
+	############
+	# diversos #
+	############
+	def imprimepilha(pilha):
+		for i, p in enumerate(pilha):
+			print('{0}: {1}'.format(i, p))
 
-def iscomandosair(comando):
-	comando = comando.strip().lower()
-	return comando == 'exit' or comando == 'sair' or comando == 's'
+	def iscomandosair(comando):
+		comando = comando.strip().lower()
+		return comando == 'exit' or comando == 'quit' or comando == 'q'
 
-def isnumber(number):
-	return number.replace('.', '', 1).isdigit()
+	def isnumber(number):
+		numero = number.replace('.', '', 1)
 
-operacoes = {
-	'+': {
-		'operadores': 2,
-		'funcao': adicao
-	},
-	'-': {
-		'operadores': 2,
-		'funcao': subtracao
-	},
-	'*': {
-		'operadores': 2,
-		'funcao': multiplicacao
-	},
-	'/': {
-		'operadores': 2,
-		'funcao': divisao
-	},
-	'pw': {
-		'operadores': 2,
-		'funcao': power
-	},
-	'sq': {
-		'operadores': 1,
-		'funcao': square
-	},
-	'l2': {
-		'operadores': 1,
-		'funcao': log2
+		if (number.startswith('-')):
+			return numero.replace('-', '', 1).isdigit()
+
+		return numero.isdigit()
+
+	#############
+	# operacoes #
+	#############
+	operacoes = {
+		'+': {
+			'operadores': 2,
+			'funcao': adicao
+		},
+		'-': {
+			'operadores': 2,
+			'funcao': subtracao
+		},
+		'*': {
+			'operadores': 2,
+			'funcao': multiplicacao
+		},
+		'/': {
+			'operadores': 2,
+			'funcao': divisao
+		},
+		'pw': {
+			'operadores': 2,
+			'funcao': power
+		},
+		'sq': {
+			'operadores': 1,
+			'funcao': square
+		},
+		'l2': {
+			'operadores': 1,
+			'funcao': log2
+		}
 	}
-}
 
-while True:
-	leitura = input('>>>')
+	#############
+	# main loop #
+	#############
 
-	operacao = operacoes.get(leitura)
+	while True:
+		leitura = input('>>>')
 
-	if operacao:
-		operadores = operacao.get('operadores')
+		operacao = operacoes.get(leitura)
 
-		if len(pilha) >= operadores:
-			funcao = operacao.get('funcao')
+		if operacao:
+			operadores = operacao.get('operadores')
 
-			valor = funcao(*pilha[-operadores:])
-			pilha = pilha[0:len(pilha) - operadores]
-			pilha.append(valor)
+			if len(pilha) >= operadores:
+				funcao = operacao.get('funcao')
+
+				valor = funcao(*pilha[-operadores:])
+				pilha = pilha[0:len(pilha) - operadores]
+				pilha.append(valor)
+			else:
+				print('pilha sem elementos para operacao')
 		else:
-			print('pilha sem elementos para operacao')
-	else:
-		if iscomandosair(leitura):
-			break
+			if iscomandosair(leitura):
+				break
 
-		if isnumber(leitura):
-			valor = float(leitura)
-			pilha.append(valor)
-		else:
-			print('valor invalido: \'{0}\''.format(leitura))
+			if isnumber(leitura):
+				valor = float(leitura)
+				pilha.append(valor)
+			else:
+				print('valor invalido: \'{0}\''.format(leitura))
 
-	imprimepilha(pilha)
+		imprimepilha(pilha)
+
+if __name__  == '__main__':
+	main()
